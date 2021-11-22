c334a632371a899eb359075b16c52d1ca4b10da8
Gene2004a <58836434+Gene2004a@users.noreply.github.com>
2021-11-22 19:20:58 +0800

    Update Preprocess.py

--- Train/Preprocess.py
+++ Train/Preprocess.py
@@ -11,7 +11,7 @@ SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
 
 def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=1024, hop_length=512, num_segments=5):
-    # dictionary to store mapping, labels, and MFCCs
+    # mapping, labels, and MFCCs in the dictionary
     data = {
         "mapping": [],
         "labels": [],
@@ -21,42 +21,39 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=1024, hop_length=512, 
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
     num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
 
-    # loop through all genre sub-folder
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
-
         # ensure we're processing a genre sub-folder level
         if dirpath is not dataset_path:
             
-            # save genre label (i.e., sub-folder name) in the mapping
+            # mapping the labels to the bird species
             semantic_label = dirpath.split("/")[-1]
             data["mapping"].append(semantic_label)
             print("\nProcessing: {}".format(semantic_label))
-
-            # process all audio files in genre sub-dir
+	
             for f in filenames:
-
 		# load audio file
                 file_path = os.path.join(dirpath, f)
                 signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
 
                 # process all segments of audio file
-                for d in range(num_segments):
+                for index in range(num_segments):
 
-                    # calculate start and finish sample for current segment
-                    start = samples_per_segment * d
+                    # split the index from the start to the end (as the following formulas)
+                    start = samples_per_segment * index
                     finish = start + samples_per_segment
 
-                    # extract mfcc
+                    # extract mfcc features
                     mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
                     mfcc = mfcc.T
 
-                    # store only mfcc feature with expected number of vectors
                     if len(mfcc) == num_mfcc_vectors_per_segment:
                         data["mfcc"].append(mfcc.tolist())
                         data["labels"].append(i-1)
+			#Storing into json files
                         print("{}, segment:{}".format(file_path, d+1))
+			
 
-    # save MFCCs to json file
+    # save features in the json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
         
